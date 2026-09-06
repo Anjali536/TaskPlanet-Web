e86ff78b692f56c79c46f4de549476e3ad390bac
Anjali Thakur <Sarojini46k@gmail.com>
2026-09-06 16:44:12 +0530

    Modified frontend for deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = "https://taskplanet-web1.onrender.com/api";
+const API_URL = import.meta.env.VITE_API_URL;
 
 async function request(endpoint, options = {}) {
     const token = localStorage.getItem("token");
